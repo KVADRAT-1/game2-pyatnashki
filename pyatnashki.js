0e8d4886b89d83ce04f84ad395199be5be457f5e
KVADRAT-1 <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-07-14 14:12:27 +0300

    14.07.2021/2

--- pyatnashki.js
+++ pyatnashki.js
@@ -4,28 +4,44 @@ const gameUserWin = game.querySelector('.game__user-win');
 const gameWindow = game.querySelector('.game__window');
 const gameCellAll = game.querySelectorAll('.game__cell');
 const gameResetButton = game.querySelector('.game__reset-button');
+let columnsNumber = 0;
+let stringNumber = 0;
 
-let number = 15
+let win = [[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, '']];
 
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * max);
-// }
+let cellNumber = [[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, '']];
 
-// console.log(getRandomInt(number))
+function addNumbers() {
+    gameCellAll.forEach((item) => {
+        if (columnsNumber > 3) {
+            columnsNumber = 0; 
+            stringNumber++;
+            item.textContent = cellNumber[stringNumber][columnsNumber];
+            columnsNumber++;
+            return;
+        }
+        item.textContent = cellNumber[stringNumber][columnsNumber];
+        columnsNumber++;
+        if (columnsNumber === 4 && stringNumber === 3) {
+            columnsNumber = 0;
+            stringNumber = 0;
+        }
+    });
+}
 
-let cellNumber = [{number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''}, {number: ''},];
-
-cellNumber.forEach((cell) => {
-    if (number <= 0) {
-        number = ''
-        cell.number = `${number}`;
-        return
+function goNumber(e) {
+    if (e.target.classList[0] === 'game__window') { return }
+    for (stringNumber = 0; stringNumber < 4; stringNumber++) {
+        for (columnsNumber = 0; columnsNumber < 4; columnsNumber++) {
+            if (cellNumber[stringNumber][columnsNumber] == e.target.textContent) {
+                console.log(`string: ${stringNumber}`)
+                console.log(`column: ${columnsNumber}`)
+            }
+        }
     }
-    cell.number = `${number}`;
-    number -= 1;
-})
+}
 
-console.log(cellNumber)
+addNumbers()
 
-// gameWindow.addEventListener('click', xxx);
-// gameResetButton.addEventListener('click', xxx);
+gameWindow.addEventListener('click', goNumber);
+gameResetButton.addEventListener('click', addNumbers);
